4bc2903faad14e2e7b703d97104b4fab20b2f7a3
waclaw stobierski <waclaw.stobierski@gmail.com>
2020-05-25 16:57:40 +0200

    Screenshots changes for MyAccountSubpageTest

--- src/test/java/tests/MyAccountSubpageTest.java
+++ src/test/java/tests/MyAccountSubpageTest.java
@@ -4,6 +4,8 @@ import org.testng.Assert;
 import org.testng.annotations.Test;
 import pages.*;
 
+import java.util.Date;
+
 public class MyAccountSubpageTest extends  DefaultTest{
 
 
@@ -13,6 +15,11 @@ public class MyAccountSubpageTest extends  DefaultTest{
                 new DashboardPage(driver, logger)
                 .goToOrderHistoryPage()
                 .isOrderHistoryPageDisplayed());
+        customScreenshot.makeScreenshot(driver,
+                new Date().getTime() + "_" +
+                        new LoginTest() {
+                        }.getClass().getEnclosingMethod().getName()
+                        + "_" + "ORDERHISTORY PAGE SCREEN");
         OrderHistoryPage orderHistoryPage = new OrderHistoryPage(driver, logger);
         orderHistoryPage.backToYourAccount();
     }
@@ -23,6 +30,11 @@ public class MyAccountSubpageTest extends  DefaultTest{
                 new DashboardPage(driver, logger)
                 .goToMyCreditPage()
                 .isMyCreditPageDisplayed());
+        customScreenshot.makeScreenshot(driver,
+                new Date().getTime() + "_" +
+                        new LoginTest() {
+                        }.getClass().getEnclosingMethod().getName()
+                        + "_" + "MYCREDIT PAGE SCREEN");
         MyCreditPage myCreditPage = new MyCreditPage(driver, logger);
         myCreditPage.backToYourAccount();
     }
@@ -33,6 +45,11 @@ public class MyAccountSubpageTest extends  DefaultTest{
                 new DashboardPage(driver, logger)
                 .goToMyAddressesPage()
                 .isMyAddressesPageDisplayed());
+        customScreenshot.makeScreenshot(driver,
+                new Date().getTime() + "_" +
+                        new LoginTest() {
+                        }.getClass().getEnclosingMethod().getName()
+                        + "_" + "ADDRESSES PAGE SCREEN");
         MyAddressesPage myAddressesPage = new MyAddressesPage(driver, logger);
         myAddressesPage.backToYourAccount();
     }
@@ -43,6 +60,11 @@ public class MyAccountSubpageTest extends  DefaultTest{
                 new DashboardPage(driver, logger)
                 .goToPersonalInfoPage()
                 .isPersonalInfoPageDisplayed());
+        customScreenshot.makeScreenshot(driver,
+                new Date().getTime() + "_" +
+                        new LoginTest() {
+                        }.getClass().getEnclosingMethod().getName()
+                        + "_" + "PERSONAL INFO PAGE SCREEN");
                 PersonalInfoPage personalInfoPage = new PersonalInfoPage(driver, logger);
                 personalInfoPage.backToYourAccount();
     }
@@ -53,6 +75,11 @@ public class MyAccountSubpageTest extends  DefaultTest{
                 new DashboardPage(driver, logger)
                 .goToWishListPage()
                 .isWishListPageDisplayed());
+        customScreenshot.makeScreenshot(driver,
+                new Date().getTime() + "_" +
+                        new LoginTest() {
+                        }.getClass().getEnclosingMethod().getName()
+                        + "_" + "WISHLIST PAGE SCREEN");
         WishListPage wishListPage = new WishListPage(driver, logger);
         wishListPage.backToYourAccount();
     }
